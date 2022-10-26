6ffebd195094db91eb5fa2d6e871590e30c6aca6
vux <vucqhe150798@fpt.edu.vn>
2022-10-26 10:18:34 +0700

    attended screen

---
+++ PRJ301_ASG/src/java/controller/lecturer/AttController.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package controller.lecturer;
+
+import dal.SessionDBContext;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.Attandance;
+import model.Session;
+import model.Student;
+
+/**
+ *
+ * @author sonnt
+ */
+public class AttController extends HttpServlet {
+   
+    
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        int sesid = Integer.parseInt(request.getParameter("id"));
+        SessionDBContext sesDB = new SessionDBContext();
+        Session ses = sesDB.get(sesid);
+        request.setAttribute("ses", ses);
+        request.getRequestDispatcher("../view/lecturer/att.jsp").forward(request, response);
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        Session ses = new Session();
+        ses.setId(Integer.parseInt(request.getParameter("sesid")));
+        String[] stdids = request.getParameterValues("stdid");
+        for (String stdid : stdids) {
+            Attandance a =new Attandance();
+            Student s = new Student();
+            a.setStudent(s);
+            a.setDescription(request.getParameter("description"+stdid));
+            a.setPresent(request.getParameter("present"+stdid).equals("present"));
+            s.setId(Integer.parseInt(stdid));
+            ses.getAtts().add(a);
+        }
+        SessionDBContext db = new SessionDBContext();
+        db.update(ses);
+        response.sendRedirect("takeatt?id="+ses.getId());
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ PRJ301_ASG/src/java/dal/SessionDBContext.java
@@ -0,0 +1,173 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import controller.lecturer.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Attandance;
+import model.Group;
+import model.Lecturer;
+import model.Room;
+import model.Session;
+import model.Student;
+import model.Subject;
+import model.TimeSlot;
+
+/**
+ *
+ * @author sonnt
+ */
+public class SessionDBContext extends DBContext<Session> {
+
+    @Override
+    public void insert(Session model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void update(Session model) {
+        try {
+            connection.setAutoCommit(false);
+            String sql = "UPDATE [Session] SET attanded = 1 WHERE sesid = ?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setInt(1, model.getId());
+            stm.executeUpdate();
+
+            //remove old attandances
+            sql = "DELETE Attandance WHERE sesid = ?";
+            PreparedStatement stm_delete = connection.prepareStatement(sql);
+            stm_delete.setInt(1, model.getId());
+            stm_delete.executeUpdate();
+
+            //insert new attandances
+            for (Attandance att : model.getAtts()) {
+                sql = "INSERT INTO [Attandance]\n"
+                        + "           ([sesid]\n"
+                        + "           ,[stdid]\n"
+                        + "           ,[present]\n"
+                        + "           ,[description]\n"
+                       
+                        + "     VALUES\n"
+                        + "           (?\n"
+                        + "           ,?\n"
+                        + "           ,?\n"
+                        + "           ,?\n";
+                PreparedStatement stm_insert = connection.prepareStatement(sql);
+                stm_insert.setInt(1, model.getId());
+                stm_insert.setInt(2, att.getStudent().getId());
+                stm_insert.setBoolean(3, att.isPresent());
+                stm_insert.setString(4, att.getDescription());
+                stm_insert.executeUpdate();
+            }
+            connection.commit();
+        } catch (SQLException ex) {
+            try {
+                connection.rollback();
+            } catch (SQLException ex1) {
+                Logger.getLogger(SessionDBContext.class.getName()).log(Level.SEVERE, null, ex1);
+            }
+            Logger.getLogger(SessionDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                connection.setAutoCommit(true);
+            } catch (SQLException ex) {
+                Logger.getLogger(SessionDBContext.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+    }
+
+    @Override
+    public void delete(Session model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Session get(int id) {
+        try {
+            String sql = "SELECT ses.sesid,ses.[index],ses.date,ses.attanded\n"
+                    + "	,g.gid,g.gname\n"
+                    + "	,r.rid,r.rname\n"
+                    + "	,t.tid,t.[description] tdescription\n"
+                    + "	,l.lid,l.lname\n"
+                    + "	,sub.subid,sub.subname\n"
+                    + "	,s.stdid,s.stdname\n"
+                    + "	,ISNULL(a.present,0) present, ISNULL(a.[description],'') [description]\n"
+                    + "		FROM [Session] ses\n"
+                    + "		INNER JOIN Room r ON r.rid = ses.rid\n"
+                    + "		INNER JOIN TimeSlot t ON t.tid = ses.tid\n"
+                    + "		INNER JOIN Lecturer l ON l.lid = ses.lid\n"
+                    + "		INNER JOIN [Group] g ON g.gid = ses.gid\n"
+                    + "		INNER JOIN [Subject] sub ON sub.subid = g.subid\n"
+                    + "		INNER JOIN [Student_Group] sg ON sg.gid = g.gid\n"
+                    + "		INNER JOIN [Student] s ON s.stdid = sg.stdid\n"
+                    + "		LEFT JOIN Attandance a ON s.stdid = a.stdid AND ses.sesid = a.sesid\n"
+                    + "WHERE ses.sesid = ?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setInt(1, id);
+            ResultSet rs = stm.executeQuery();
+            Session ses = null;
+            while (rs.next()) {
+                if (ses == null) {
+                    ses = new Session();
+                    Room r = new Room();
+                    r.setId(rs.getInt("rid"));
+                    r.setName(rs.getString("rname"));
+                    ses.setRoom(r);
+
+                    TimeSlot t = new TimeSlot();
+                    t.setId(rs.getInt("tid"));
+                    t.setDescription(rs.getString("tdescription"));
+                    ses.setSlot(t);
+
+                    Lecturer l = new Lecturer();
+                    l.setId(rs.getInt("lid"));
+                    l.setName(rs.getString("lname"));
+                    ses.setLecturer(l);
+
+                    Group g = new Group();
+                    g.setId(rs.getInt("gid"));
+                    g.setName(rs.getString("gname"));
+                    ses.setGroup(g);
+
+                    Subject sub = new Subject();
+                    sub.setId(rs.getInt("subid"));
+                    sub.setName(rs.getString("subname"));
+                    g.setSubject(sub);
+
+                    ses.setDate(rs.getDate("date"));
+                    ses.setIndex(rs.getInt("index"));
+                    ses.setAttanded(rs.getBoolean("attanded"));
+                }
+                //read student
+                Student s = new Student();
+                s.setId(rs.getInt("stdid"));
+                s.setName(rs.getString("stdname"));
+                //read attandance
+                Attandance a = new Attandance();
+                a.setStudent(s);
+                a.setSession(ses);
+                a.setPresent(rs.getBoolean("present"));
+                a.setDescription(rs.getString("description"));
+                ses.getAtts().add(a);
+            }
+            return ses;
+        } catch (SQLException ex) {
+            Logger.getLogger(SessionDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    @Override
+    public ArrayList<Session> list() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
